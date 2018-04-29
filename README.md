c744365f2821b2232f66d49b911f7fbfb24780aa
Tonsai jompun <tonsai.j@nexts-corp.com>
2018-04-29 18:47:06 +0700

    อัพเดท injectShadyDom

--- README.md
+++ README.md
@@ -4,4 +4,8 @@
 3. npm run start
 
 ใช้งานได้
+# ทดสอบเรนเดอร์
+1. https://technicalseo.com/seo-tools/fetch-render/
+2. https://cards-dev.twitter.com/validator
+3. https://developers.facebook.com/tools/debug/
 
--- server.js
+++ server.js
@@ -4,17 +4,19 @@ const rendertron = require('rendertron-middleware');
 const config = require('./build/polymer.json');
 const app = express();
 
-// const botUserAgents = [
-//     'W3C_Validator',
-//     'baiduspider',
-//     'bingbot',
-//     'googlebot',
-// ];
-// const botList = new RegExp(botUserAgents.join('|'), 'i');
+const botUserAgents = [
+    'W3C_Validator',
+    'baiduspider',
+    'bingbot',
+    'googlebot',
+];
+const botList = new RegExp(botUserAgents.join('|'), 'i');
 app.use(rendertron.makeMiddleware({
     proxyUrl: 'https://render-tron.appspot.com/render',
-    // userAgentPattern: botList,
+    userAgentPattern: botList,
+    injectShadyDom: true, // สำหรับ webcomponents ต้องใส่ด้วย
 }));
+
 app.get('/*', prpl.makeHandler('./build/', config));
 
 app.listen(8080);
